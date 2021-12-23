dee4916eee85bed34ebb3652e4c5e78f93c6825c
Cedric Vallieu <cvallieu94@gmail.com>
2021-12-22 20:56:18 -0800

    fixed syntax for netlify

--- src/FilterBeast.js
+++ src/FilterBeast.js
@@ -1,13 +1,7 @@
 import React from 'react';
-// import { json } from './Main';
 import json from './data.json';
 
-
-
 class FilterBeast extends React.Component {
-
-  
-
   handleChange = (e) => {
     console.log(e.target.value);
     const selection = e.target.value;
@@ -15,18 +9,17 @@ class FilterBeast extends React.Component {
     console.log(json);
     if (selection === 'one') {
       updatingbeast = json.filter(element => element.horns === 1);
-    } else if (selection === 'two') { 
+    } else if (selection === 'two') {
       updatingbeast = json.filter(element => element.horns === 2);
-    } else if (selection === 'three') { 
+    } else if (selection === 'three') {
       updatingbeast = json.filter(element => element.horns === 3);
-    } else if (selection === 'one hundred') { 
+    } else if (selection === 'one hundred') {
       updatingbeast = json.filter(element => element.horns === 100);
-    } else { 
+    } else {
       updatingbeast = json;
     }
     this.props.updateBeast(updatingbeast);
   }
-
   render() {
     return (
       <>
